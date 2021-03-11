369e22f21689b2bd347fbcbb1b007e0926d91de4
Bioinformatics-analysis <80457309+Bioinformatics-analysis@users.noreply.github.com>
2021-03-11 20:19:08 +0800

    Add files via upload

---
+++ Independent Validation/Independent_validation.R
@@ -0,0 +1,188 @@
+library(survival)                    
+# Train Forest analysis
+rt=read.table("Train_independent.txt",header=T,sep="\t",check.names=F,row.names=1)            
+outTab=data.frame()
+for(i in colnames(rt[,3:ncol(rt)])){
+	 cox <- coxph(Surv(futime, fustat) ~ rt[,i], data = rt)
+	 coxSummary = summary(cox)
+	 coxP=coxSummary$coefficients[,"Pr(>|z|)"]
+	 outTab=rbind(outTab,
+	              cbind(id=i,
+	              HR=coxSummary$conf.int[,"exp(coef)"],
+	              HR.95L=coxSummary$conf.int[,"lower .95"],
+	              HR.95H=coxSummary$conf.int[,"upper .95"],
+	              pvalue=coxSummary$coefficients[,"Pr(>|z|)"])
+	              )
+}
+write.table(outTab,file="Train_uniCox.txt",sep="\t",row.names=F,quote=F)
+
+rt <- read.table("Train_uniCox.txt",header=T,sep="\t",row.names=1,check.names=F)
+gene <- rownames(rt)
+hr <- sprintf("%.3f",rt$"HR")
+hrLow  <- sprintf("%.3f",rt$"HR.95L")
+hrHigh <- sprintf("%.3f",rt$"HR.95H")
+Hazard.ratio <- paste0(hr,"(",hrLow,"-",hrHigh,")")
+pVal <- ifelse(rt$pvalue<0.001, "<0.001", sprintf("%.3f", rt$pvalue))
+
+pdf(file="Train-Unicox_forest.pdf", width = 7,height = 4)
+n <- nrow(rt)
+nRow <- n+1
+ylim <- c(1,nRow)
+layout(matrix(c(1,2),nc=2),width=c(3,2.5))
+
+xlim = c(0,3)
+par(mar=c(4,2.5,2,1))
+plot(1,xlim=xlim,ylim=ylim,type="n",axes=F,xlab="",ylab="")
+text.cex=0.8
+text(0,n:1,gene,adj=0,cex=text.cex)
+text(1.5-0.5*0.2,n:1,pVal,adj=1,cex=text.cex);text(1.5-0.5*0.2,n+1,'pvalue',cex=text.cex,font=2,adj=1)
+text(3,n:1,Hazard.ratio,adj=1,cex=text.cex);text(3,n+1,'Hazard ratio',cex=text.cex,font=2,adj=1,)
+
+par(mar=c(4,1,2,1),mgp=c(2,0.5,0))
+xlim = c(0,max(as.numeric(hrLow),as.numeric(hrHigh)))
+plot(1,xlim=xlim,ylim=ylim,type="n",axes=F,ylab="",xaxs="i",xlab="Hazard ratio")
+arrows(as.numeric(hrLow),n:1,as.numeric(hrHigh),n:1,angle=90,code=3,length=0.05,col="darkblue",lwd=2.5)
+abline(v=1,col="black",lty=2,lwd=2)
+boxcolor = ifelse(as.numeric(hr) > 1, 'red', 'green')
+points(as.numeric(hr), n:1, pch = 15, col = boxcolor, cex=1.3)
+axis(1)
+dev.off()
+
+multiCox=coxph(Surv(futime, fustat) ~ ., data = rt)
+multiCoxSum=summary(multiCox)
+
+outTab=data.frame()
+outTab=cbind(
+             HR=multiCoxSum$conf.int[,"exp(coef)"],
+             HR.95L=multiCoxSum$conf.int[,"lower .95"],
+             HR.95H=multiCoxSum$conf.int[,"upper .95"],
+             pvalue=multiCoxSum$coefficients[,"Pr(>|z|)"])
+outTab=cbind(id=row.names(outTab),outTab)
+write.table(outTab,file="Train_multiCox.xls",sep="\t",row.names=F,quote=F)
+
+
+rt <- read.table("Train_multiCox.xls",header=T,sep="\t",row.names=1,check.names=F)
+gene <- rownames(rt)
+hr <- sprintf("%.3f",rt$"HR")
+hrLow  <- sprintf("%.3f",rt$"HR.95L")
+hrHigh <- sprintf("%.3f",rt$"HR.95H")
+Hazard.ratio <- paste0(hr,"(",hrLow,"-",hrHigh,")")
+pVal <- ifelse(rt$pvalue<0.001, "<0.001", sprintf("%.3f", rt$pvalue))
+
+pdf(file="Train-Multicox_forest.pdf", width = 7,height = 4)
+n <- nrow(rt)
+nRow <- n+1
+ylim <- c(1,nRow)
+layout(matrix(c(1,2),nc=2),width=c(3,2.5))
+
+xlim = c(0,3)
+par(mar=c(4,2.5,2,1))
+plot(1,xlim=xlim,ylim=ylim,type="n",axes=F,xlab="",ylab="")
+text.cex=0.8
+text(0,n:1,gene,adj=0,cex=text.cex)
+text(1.5-0.5*0.2,n:1,pVal,adj=1,cex=text.cex);text(1.5-0.5*0.2,n+1,'pvalue',cex=text.cex,font=2,adj=1)
+text(3,n:1,Hazard.ratio,adj=1,cex=text.cex);text(3,n+1,'Hazard ratio',cex=text.cex,font=2,adj=1,)
+
+par(mar=c(4,1,2,1),mgp=c(2,0.5,0))
+xlim = c(0,max(as.numeric(hrLow),as.numeric(hrHigh)))
+plot(1,xlim=xlim,ylim=ylim,type="n",axes=F,ylab="",xaxs="i",xlab="Hazard ratio")
+arrows(as.numeric(hrLow),n:1,as.numeric(hrHigh),n:1,angle=90,code=3,length=0.05,col="darkblue",lwd=2.5)
+abline(v=1,col="black",lty=2,lwd=2)
+boxcolor = ifelse(as.numeric(hr) > 1, 'red', 'green')
+points(as.numeric(hr), n:1, pch = 15, col = boxcolor, cex=1.3)
+axis(1)
+dev.off()
+
+#Test Forest analysis
+rt=read.table("Test_independent.txt",header=T,sep="\t",check.names=F,row.names=1)            
+outTab=data.frame()
+for(i in colnames(rt[,3:ncol(rt)])){
+	 cox <- coxph(Surv(futime, fustat) ~ rt[,i], data = rt)
+	 coxSummary = summary(cox)
+	 coxP=coxSummary$coefficients[,"Pr(>|z|)"]
+	 outTab=rbind(outTab,
+	              cbind(id=i,
+	              HR=coxSummary$conf.int[,"exp(coef)"],
+	              HR.95L=coxSummary$conf.int[,"lower .95"],
+	              HR.95H=coxSummary$conf.int[,"upper .95"],
+	              pvalue=coxSummary$coefficients[,"Pr(>|z|)"])
+	              )
+}
+write.table(outTab,file="Test_uniCox.txt",sep="\t",row.names=F,quote=F)
+
+rt <- read.table("Test_uniCox.txt",header=T,sep="\t",row.names=1,check.names=F)
+gene <- rownames(rt)
+hr <- sprintf("%.3f",rt$"HR")
+hrLow  <- sprintf("%.3f",rt$"HR.95L")
+hrHigh <- sprintf("%.3f",rt$"HR.95H")
+Hazard.ratio <- paste0(hr,"(",hrLow,"-",hrHigh,")")
+pVal <- ifelse(rt$pvalue<0.001, "<0.001", sprintf("%.3f", rt$pvalue))
+
+pdf(file="Test-Unicox_forest.pdf", width = 7,height = 4)
+n <- nrow(rt)
+nRow <- n+1
+ylim <- c(1,nRow)
+layout(matrix(c(1,2),nc=2),width=c(3,2.5))
+
+xlim = c(0,3)
+par(mar=c(4,2.5,2,1))
+plot(1,xlim=xlim,ylim=ylim,type="n",axes=F,xlab="",ylab="")
+text.cex=0.8
+text(0,n:1,gene,adj=0,cex=text.cex)
+text(1.5-0.5*0.2,n:1,pVal,adj=1,cex=text.cex);text(1.5-0.5*0.2,n+1,'pvalue',cex=text.cex,font=2,adj=1)
+text(3,n:1,Hazard.ratio,adj=1,cex=text.cex);text(3,n+1,'Hazard ratio',cex=text.cex,font=2,adj=1,)
+
+par(mar=c(4,1,2,1),mgp=c(2,0.5,0))
+xlim = c(0,max(as.numeric(hrLow),as.numeric(hrHigh)))
+plot(1,xlim=xlim,ylim=ylim,type="n",axes=F,ylab="",xaxs="i",xlab="Hazard ratio")
+arrows(as.numeric(hrLow),n:1,as.numeric(hrHigh),n:1,angle=90,code=3,length=0.05,col="darkblue",lwd=2.5)
+abline(v=1,col="black",lty=2,lwd=2)
+boxcolor = ifelse(as.numeric(hr) > 1, 'red', 'green')
+points(as.numeric(hr), n:1, pch = 15, col = boxcolor, cex=1.3)
+axis(1)
+dev.off()
+
+multiCox=coxph(Surv(futime, fustat) ~ ., data = rt)
+multiCoxSum=summary(multiCox)
+
+outTab=data.frame()
+outTab=cbind(
+             HR=multiCoxSum$conf.int[,"exp(coef)"],
+             HR.95L=multiCoxSum$conf.int[,"lower .95"],
+             HR.95H=multiCoxSum$conf.int[,"upper .95"],
+             pvalue=multiCoxSum$coefficients[,"Pr(>|z|)"])
+outTab=cbind(id=row.names(outTab),outTab)
+write.table(outTab,file="Test_multiCox.xls",sep="\t",row.names=F,quote=F)
+
+
+rt <- read.table("Test_multiCox.xls",header=T,sep="\t",row.names=1,check.names=F)
+gene <- rownames(rt)
+hr <- sprintf("%.3f",rt$"HR")
+hrLow  <- sprintf("%.3f",rt$"HR.95L")
+hrHigh <- sprintf("%.3f",rt$"HR.95H")
+Hazard.ratio <- paste0(hr,"(",hrLow,"-",hrHigh,")")
+pVal <- ifelse(rt$pvalue<0.001, "<0.001", sprintf("%.3f", rt$pvalue))
+
+pdf(file="Test-Multicox_forest.pdf", width = 7,height = 4)
+n <- nrow(rt)
+nRow <- n+1
+ylim <- c(1,nRow)
+layout(matrix(c(1,2),nc=2),width=c(3,2.5))
+
+xlim = c(0,3)
+par(mar=c(4,2.5,2,1))
+plot(1,xlim=xlim,ylim=ylim,type="n",axes=F,xlab="",ylab="")
+text.cex=0.8
+text(0,n:1,gene,adj=0,cex=text.cex)
+text(1.5-0.5*0.2,n:1,pVal,adj=1,cex=text.cex);text(1.5-0.5*0.2,n+1,'pvalue',cex=text.cex,font=2,adj=1)
+text(3,n:1,Hazard.ratio,adj=1,cex=text.cex);text(3,n+1,'Hazard ratio',cex=text.cex,font=2,adj=1,)
+
+par(mar=c(4,1,2,1),mgp=c(2,0.5,0))
+xlim = c(0,max(as.numeric(hrLow),as.numeric(hrHigh)))
+plot(1,xlim=xlim,ylim=ylim,type="n",axes=F,ylab="",xaxs="i",xlab="Hazard ratio")
+arrows(as.numeric(hrLow),n:1,as.numeric(hrHigh),n:1,angle=90,code=3,length=0.05,col="darkblue",lwd=2.5)
+abline(v=1,col="black",lty=2,lwd=2)
+boxcolor = ifelse(as.numeric(hr) > 1, 'red', 'green')
+points(as.numeric(hr), n:1, pch = 15, col = boxcolor, cex=1.3)
+axis(1)
+dev.off()
